a45f326200aa1674c599663e124f5012235c76cd
rckhz <alejandrolima123@gmail.com>
2026-05-06 20:55:15 -0300

    inicio do projeto

---
+++ calculadora/Models/CalculadoraModel.cs
@@ -0,0 +1,22 @@
+﻿namespace calculadora.Models
+{
+    public class CalculadoraModel
+    {
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+        public string Operador { get; set; }
+        public double Resultado { get; set; }
+
+        public void Calcular()
+        {
+            Resultado = Operador switch
+            {
+                "+" => Numero1 + Numero2,
+                "-" => Numero1 - Numero2,
+                "*" => Numero1 * Numero2,
+                "/" => Numero2 != 0 ? Numero1 / Numero2 : 0,
+                _ => 0
+            };
+        }
+    }
+}
